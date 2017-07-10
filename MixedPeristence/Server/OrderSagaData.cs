44caad142002020f09359fdd0124e5f5801589cf
Simon Cropp <simon.cropp@gmail.com>
2017-07-10 10:23:31 +1000

    add mixed persistence

---
+++ MixedPeristence/Server/OrderSagaData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using NServiceBus;
+
+public class OrderSagaData :
+    ContainSagaData
+{
+    public virtual Guid OrderId { get; set; }
+    public virtual string OrderDescription { get; set; }
+}
