237737e0a9e1ac7d12f6e2255afbea397924f050
Simon Cropp <simon.cropp@gmail.com>
2017-07-25 10:13:14 +1000

    add mixed mode assemblyname

---
+++ MixedModeAssemblyName/X86Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        var token = AssemblyName.GetAssemblyName(@"C:\Code\scratch\MixedModeAssemblyName\X64Console\bin\Debug\X64.exe").GetPublicKeyToken();
+        var publicKeyToken = string.Concat(token.Select(i => i.ToString("x2")));
+        Console.WriteLine(publicKeyToken);
+        Console.ReadLine();
+    }
+}
