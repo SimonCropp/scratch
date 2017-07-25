b20c14e79bebf94cc42bc9454539c5c396b508d9
Simon Cropp <simon.cropp@gmail.com>
2017-07-25 10:26:48 +1000

    add new csproj system

--- MixedModeAssemblyName/X86Console/Program.cs
+++ MixedModeAssemblyName/X86Console/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main()
     {
-        var token = AssemblyName.GetAssemblyName(@"C:\Code\scratch\MixedModeAssemblyName\X64Console\bin\Debug\X64.exe").GetPublicKeyToken();
+        var token = AssemblyName.GetAssemblyName(@"C:\Code\scratch\MixedModeAssemblyName\X64Console\bin\Debug\X64Console.exe").GetPublicKeyToken();
         var publicKeyToken = string.Concat(token.Select(i => i.ToString("x2")));
         Console.WriteLine(publicKeyToken);
         Console.ReadLine();
--- MixedModeAssemblyName/X86ConsoleNew/Program.cs
+++ MixedModeAssemblyName/X86ConsoleNew/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main()
     {
-        var token = AssemblyName.GetAssemblyName(@"C:\Code\scratch\MixedModeAssemblyName\X86Console\bin\Debug\X86Console.exe").GetPublicKeyToken();
+        var token = AssemblyName.GetAssemblyName(@"C:\Code\scratch\MixedModeAssemblyName\X64ConsoleNew\bin\Debug\net452\X64ConsoleNew.exe").GetPublicKeyToken();
         var publicKeyToken = string.Concat(token.Select(i => i.ToString("x2")));
         Console.WriteLine(publicKeyToken);
         Console.ReadLine();
