477e7f37272b617eb347657c7e9d664f26be1dd6
Simon Cropp <simon.cropp@gmail.com>
2018-07-06 16:25:13 +1000

    .

---
+++ ExpressionBuilder/ExpressionBuilder/Person.cs
@@ -0,0 +1,5 @@
+﻿public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ ExpressionBuilder/ExpressionBuilder/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+public class Tests
+{
+    [Theory]
+    [InlineData("Name", "==", "Person 1", "Person 1")]
+    [InlineData("Name", "!=", "Person 2", "Person 1")]
+    [InlineData("Name", "Contains", "son 2", "Person 2")]
+    [InlineData("Name", "StartsWith", "Person 2", "Person 2")]
+    [InlineData("Name", "EndsWith", "son 2", "Person 2")]
+    [InlineData("Age", "==", "13", "Person 2")]
+    [InlineData("Age", ">", "12", "Person 2")]
+    [InlineData("Age", "!=", "12", "Person 2")]
+    [InlineData("Age", ">=", "13", "Person 2")]
+    [InlineData("Age", "<", "13", "Person 1")]
+    [InlineData("Age", "<=", "12", "Person 1")]
+    public void Foo(string name, string expression, string value, string expectedName)
+    {
+        var people = new List<Person>
+        {
+            new Person
+            {
+                Name = "Person 1",
+                Age = 12
+            },
+            new Person
+            {
+                Name = "Person 2",
+                Age = 13
+            }
+        };
+
+        var result = people.AsQueryable()
+            .Where(ExpressionUtils.BuildPredicate<Person>(name, expression, value))
+            .Single();
+        Assert.Equal(expectedName, result.Name);
+    }
+}
