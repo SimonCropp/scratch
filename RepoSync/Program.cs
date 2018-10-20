df87de2acc685edd3e79bbb285afb587350d9bc2
simoncropp <simon.cropp@gmail.com>
2018-10-20 12:29:01 +1100

    Update RepoSync/Program.cs

--- RepoSync/Program.cs
+++ RepoSync/Program.cs
@@ -15,6 +15,18 @@ class Program
         sync.AddSourceItem(TreeEntryTargetType.Blob, "RepoSync/Source/ISSUE_TEMPLATE/bug_report.md", ".github/ISSUE_TEMPLATE/bug_report.md");
         sync.AddSourceItem(TreeEntryTargetType.Blob, "RepoSync/Source/ISSUE_TEMPLATE/feature_request.md", ".github/ISSUE_TEMPLATE/feature_request.md");
         sync.AddTarget("SimonCropp", "CaptureSnippets", "master");
+        sync.AddTarget("SimonCropp", "WaffleGenerator", "master");
+        sync.AddTarget("SimonCropp", "Timestamp", "master");
+        sync.AddTarget("SimonCropp", "TextCopy", "master");
+        sync.AddTarget("SimonCropp", "SetStartupProjects", "master");
+        sync.AddTarget("SimonCropp", "ObjectApproval", "master");
+        sync.AddTarget("SimonCropp", "NodaTime.Bogus", "master");
+        sync.AddTarget("SimonCropp", "Newtonsoft.Json.Encryption", "master");
+        sync.AddTarget("SimonCropp", "NaughtyStrings", "master");
+        sync.AddTarget("SimonCropp", "GraphQL.EntityFramework", "master");
+        sync.AddTarget("SimonCropp", "GitModTimes", "master");
+        sync.AddTarget("SimonCropp", "EfCore.InMemoryHelpers", "master");
+        sync.AddTarget("SimonCropp", "CountryData", "master");
         return sync.Sync();
     }
 }
