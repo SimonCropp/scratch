d1b5082cc89cad49073c318b078ac2107ac1526b
simoncropp <simon.cropp@gmail.com>
2018-10-20 12:10:29 +1100

    add reposync

---
+++ RepoSync/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using GitHubSync;
+using Octokit;
+
+class Program
+{
+    static Task Main()
+    {
+        var githubToken = Environment.GetEnvironmentVariable("Octokit_OAuthToken");
+
+        var credentials = new Credentials(githubToken);
+        var sync = new RepoSync(credentials, "SimonCropp", "scratch", "master", Console.WriteLine);
+        sync.AddSourceItem(TreeEntryTargetType.Blob, "RepoSync/Source/.editorconfig", ".editorconfig");
+        sync.AddSourceItem(TreeEntryTargetType.Blob, "RepoSync/Source/ISSUE_TEMPLATE/bug_report.md", ".github/ISSUE_TEMPLATE/bug_report.md");
+        sync.AddSourceItem(TreeEntryTargetType.Blob, "RepoSync/Source/ISSUE_TEMPLATE/feature_request.md", ".github/ISSUE_TEMPLATE/feature_request.md");
+        sync.AddTarget("SimonCropp", "CaptureSnippets", "master");
+        return sync.Sync();
+    }
+}
