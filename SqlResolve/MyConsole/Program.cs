0327b5d6c8db90b306b4487e87ddc354835967d0
Simon Cropp <simon.cropp@gmail.com>
2018-11-02 16:37:33 +1100

    .

---
+++ SqlResolve/MyConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine( typeof(SqlConnection).Assembly.CodeBase);
+        Console.ReadKey();
+    }
+}
