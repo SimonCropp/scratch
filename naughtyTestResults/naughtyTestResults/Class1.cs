c3f12e1baf8c8f894d91c6c7825fdf7d0ed36172
Simon Cropp <simon.cropp@gmail.com>
2019-09-27 11:22:55 +1000

    .

---
+++ naughtyTestResults/naughtyTestResults/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NaughtyStrings;
+using Xunit;
+using Xunit.Abstractions;
+
+public class XunitUsage
+{
+    ITestOutputHelper output;
+
+    public XunitUsage(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    [Theory]
+    [MemberData(nameof(GetData))]
+    public void Run(string naughtyString)
+    {
+        output.WriteLine(naughtyString);
+    }
+
+    public static IEnumerable<object[]> GetData()
+    {
+        return TheNaughtyStrings.All
+            .Select(naughty => new object[] {naughty});
+    }
+}
